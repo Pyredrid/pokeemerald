1763b23545e0a8e5ec215a80219a369260d37ac9
Pyredrid <Pyredrid@users.noreply.github.com>
2020-09-01 15:46:31 -0400

    Added Give Item functionality

--- src/debug.c
+++ src/debug.c
@@ -1,37 +1,124 @@
 #if DEBUG
 
 #include "global.h"
+#include "event_object_movement.h"
+#include "international_string_util.h"
+#include "item.h"
 #include "list_menu.h"
 #include "main.h"
 #include "map_name_popup.h"
 #include "menu.h"
 #include "script.h"
+#include "script_pokemon_util.h"
 #include "sound.h"
 #include "strings.h"
+#include "string_util.h"
 #include "task.h"
+#include "constants/items.h"
 #include "constants/songs.h"
 
+#define DEBUG_MAIN_MENU_WIDTH 12
 #define DEBUG_MAIN_MENU_HEIGHT 7
-#define DEBUG_MAIN_MENU_WIDTH 11
+
+#define DEBUG_NUMBER_DISPLAY_WIDTH 10
+#define DEBUG_NUMBER_DISPLAY_HEIGHT 3
 
 void Debug_ShowMainMenu(void);
+
 static void Debug_DestroyMainMenu(u8);
+static void DebugAction_Flags(u8 taskId);
+static void DebugAction_Variables(u8 taskId);
+static void DebugAction_WarpToMap(u8 taskId);
+
+static void DebugAction_GiveItem(u8 taskId);
+static void GiveItem_SelectItemId(u8 taskId);
+static void GiveItem_SelectItemQuantity(u8 taskId);
+static void GiveItem_DestroySelectItem(u8 taskId);
+
+static void DebugAction_HealParty(u8 taskId);
+static void DebugAction_AccessPC(u8 taskId);
+static void DebugAction_GivePokemon(u8 taskId);
 static void DebugAction_Cancel(u8);
 static void DebugTask_HandleMainMenuInput(u8);
 
 enum {
-    DEBUG_MENU_ITEM_CANCEL,
+    DEBUG_MENU_ITEM_FLAGS,
+    DEBUG_MENU_ITEM_VARIABLES,
+    DEBUG_MENU_ITEM_WARP_TO_MAP,
+    DEBUG_MENU_ITEM_GIVE_ITEM,
+    DEBUG_MENU_ITEM_HEAL_PARTY,
+    DEBUG_MENU_ITEM_ACCESS_PC,
+    DEBUG_MENU_ITEM_GIVE_POKEMON,
+    DEBUG_MENU_ITEM_CANCEL
 };
 
-static const u8 gDebugText_Cancel[] = _("Cancel");
+static const u8 gDebugText_Flags[] =            _("Flags");
+static const u8 gDebugText_Variables[] =        _("Variables");
+static const u8 gDebugText_WarpToMap[] =        _("Warp To Map");
+static const u8 gDebugText_GiveItem[] =         _("Give Item");
+static const u8 gDebugText_HealParty[] =        _("Heal Party");
+static const u8 gDebugText_AccessPC[] =         _("Access PC");
+static const u8 gDebugText_GivePokemon[] =      _("Give Pokemon");
+static const u8 gDebugText_Cancel[] =           _("Cancel");
+
+static const u8 gText_ItemQuantity[] =  _("  Quantity:       \n  {STR_VAR_1}    \n{STR_VAR_2}");
+static const u8 gText_ItemID[] =        _("Item ID: {STR_VAR_3}\n{STR_VAR_1}    \n{STR_VAR_2}");
+
+static const u8 digitInidicator_1[] =               _("{LEFT_ARROW}x1{RIGHT_ARROW}        ");
+static const u8 digitInidicator_10[] =              _("{LEFT_ARROW}x10{RIGHT_ARROW}       ");
+static const u8 digitInidicator_100[] =             _("{LEFT_ARROW}x100{RIGHT_ARROW}      ");
+static const u8 digitInidicator_1000[] =            _("{LEFT_ARROW}x1000{RIGHT_ARROW}     ");
+static const u8 digitInidicator_10000[] =           _("{LEFT_ARROW}x10000{RIGHT_ARROW}    ");
+static const u8 digitInidicator_100000[] =          _("{LEFT_ARROW}x100000{RIGHT_ARROW}   ");
+static const u8 digitInidicator_1000000[] =         _("{LEFT_ARROW}x1000000{RIGHT_ARROW}  ");
+static const u8 digitInidicator_10000000[] =        _("{LEFT_ARROW}x10000000{RIGHT_ARROW} ");
+const u8 * const gText_DigitIndicator[] =
+{
+    digitInidicator_1,
+    digitInidicator_10,
+    digitInidicator_100,
+    digitInidicator_1000,
+    digitInidicator_10000,
+    digitInidicator_100000,
+    digitInidicator_1000000,
+    digitInidicator_10000000
+};
+
+static const s32 sPowersOfTen[] =
+{
+             1,
+            10,
+           100,
+          1000,
+         10000,
+        100000,
+       1000000,
+      10000000,
+     100000000,
+    1000000000,
+};
 
 static const struct ListMenuItem sDebugMenuItems[] =
 {
+    [DEBUG_MENU_ITEM_FLAGS] = {gDebugText_Flags, DEBUG_MENU_ITEM_FLAGS},
+    [DEBUG_MENU_ITEM_VARIABLES] = {gDebugText_Variables, DEBUG_MENU_ITEM_VARIABLES},
+    [DEBUG_MENU_ITEM_WARP_TO_MAP] = {gDebugText_WarpToMap, DEBUG_MENU_ITEM_WARP_TO_MAP},
+    [DEBUG_MENU_ITEM_GIVE_ITEM] = {gDebugText_GiveItem, DEBUG_MENU_ITEM_GIVE_ITEM},
+    [DEBUG_MENU_ITEM_HEAL_PARTY] = {gDebugText_HealParty, DEBUG_MENU_ITEM_HEAL_PARTY},
+    [DEBUG_MENU_ITEM_ACCESS_PC] = {gDebugText_AccessPC, DEBUG_MENU_ITEM_ACCESS_PC},
+    [DEBUG_MENU_ITEM_GIVE_POKEMON] = {gDebugText_GivePokemon, DEBUG_MENU_ITEM_GIVE_POKEMON},
     [DEBUG_MENU_ITEM_CANCEL] = {gDebugText_Cancel, DEBUG_MENU_ITEM_CANCEL}
 };
 
 static void (*const sDebugMenuActions[])(u8) =
 {
+    [DEBUG_MENU_ITEM_FLAGS] = DebugAction_Flags,
+    [DEBUG_MENU_ITEM_VARIABLES] = DebugAction_Variables,
+    [DEBUG_MENU_ITEM_WARP_TO_MAP] = DebugAction_WarpToMap,
+    [DEBUG_MENU_ITEM_GIVE_ITEM] = DebugAction_GiveItem,
+    [DEBUG_MENU_ITEM_HEAL_PARTY] = DebugAction_HealParty,
+    [DEBUG_MENU_ITEM_ACCESS_PC] = DebugAction_AccessPC,
+    [DEBUG_MENU_ITEM_GIVE_POKEMON] = DebugAction_GivePokemon,
     [DEBUG_MENU_ITEM_CANCEL] = DebugAction_Cancel
 };
 
@@ -65,6 +152,17 @@ static const struct ListMenuTemplate sDebugMenuListTemplate =
     .scrollMultiple = LIST_NO_MULTIPLE_SCROLL,
     .fontId = 1,
     .cursorKind = 0
+};
+
+static const struct WindowTemplate sDebugNumberDisplayWindowTemplate =
+{
+    .bg = 0,
+    .tilemapLeft = 4 + DEBUG_MAIN_MENU_WIDTH,
+    .tilemapTop = 1,
+    .width = DEBUG_NUMBER_DISPLAY_WIDTH,
+    .height = 2 * DEBUG_NUMBER_DISPLAY_HEIGHT,
+    .paletteNum = 15,
+    .baseBlock = 1,
 };
 
 void Debug_ShowMainMenu(void) {
@@ -118,6 +216,194 @@ static void DebugTask_HandleMainMenuInput(u8 taskId)
         PlaySE(SE_SELECT);
         Debug_DestroyMainMenu(taskId);
     }
+}
+
+static void DebugAction_Flags(u8 taskId)
+{
+}
+
+static void DebugAction_Variables(u8 taskId)
+{
+}
+
+static void DebugAction_WarpToMap(u8 taskId)
+{
+}
+
+static void DebugAction_GiveItem(u8 taskId)
+{
+    u8 windowId;
+
+    ClearStdWindowAndFrame(gTasks[taskId].data[1], TRUE);
+    RemoveWindow(gTasks[taskId].data[1]);
+
+    HideMapNamePopUpWindow();
+    LoadMessageBoxAndBorderGfx();
+    windowId = AddWindow(&sDebugNumberDisplayWindowTemplate);
+    DrawStdWindowFrame(windowId, FALSE);
+
+    CopyWindowToVram(windowId, 3);
+
+    //Display initial ID
+    StringCopy(gStringVar2, gText_DigitIndicator[0]);
+    ConvertIntToDecimalStringN(gStringVar3, 1, STR_CONV_MODE_LEADING_ZEROS, 4);
+    CopyItemName(1, gStringVar1);
+    StringCopyPadded(gStringVar1, gStringVar1, CHAR_SPACE, 15);
+    StringExpandPlaceholders(gStringVar4, gText_ItemID);
+    AddTextPrinterParameterized(windowId, 1, gStringVar4, 1, 1, 0, NULL);
+
+    gTasks[taskId].func = GiveItem_SelectItemId;
+    gTasks[taskId].data[2] = windowId;
+    gTasks[taskId].data[3] = 1;            //Current ID
+    gTasks[taskId].data[4] = 0;            //Digit Selected
+}
+static void GiveItem_SelectItemId(u8 taskId)
+{
+    if (gMain.newKeys & DPAD_ANY)
+    {
+        PlaySE(SE_SELECT);
+
+        if(gMain.newKeys & DPAD_UP)
+        {
+            gTasks[taskId].data[3] += sPowersOfTen[gTasks[taskId].data[4]];
+            if(gTasks[taskId].data[3] >= ITEMS_COUNT){
+                gTasks[taskId].data[3] = ITEMS_COUNT - 1;
+            }
+        }
+        if(gMain.newKeys & DPAD_DOWN)
+        {
+            gTasks[taskId].data[3] -= sPowersOfTen[gTasks[taskId].data[4]];
+            if(gTasks[taskId].data[3] < 0){
+                gTasks[taskId].data[3] = 0;
+            }
+        }
+        if(gMain.newKeys & DPAD_LEFT)
+        {
+            if(gTasks[taskId].data[4] > 0)
+            {
+                gTasks[taskId].data[4] -= 1;
+            }
+        }
+        if(gMain.newKeys & DPAD_RIGHT)
+        {
+            if(gTasks[taskId].data[4] < 3)
+            {
+                gTasks[taskId].data[4] += 1;
+            }
+        }
+
+        StringCopy(gStringVar2, gText_DigitIndicator[gTasks[taskId].data[4]]);
+        CopyItemName(gTasks[taskId].data[3], gStringVar1);
+        StringCopyPadded(gStringVar1, gStringVar1, CHAR_SPACE, 15);
+        ConvertIntToDecimalStringN(gStringVar3, gTasks[taskId].data[3], STR_CONV_MODE_LEADING_ZEROS, 4);
+        StringExpandPlaceholders(gStringVar4, gText_ItemID);
+
+        AddTextPrinterParameterized(gTasks[taskId].data[2], 1, gStringVar4, 1, 1, 0, NULL);
+    }
+
+    if (gMain.newKeys & A_BUTTON)
+    {
+        gTasks[taskId].data[5] = gTasks[taskId].data[3];
+        gTasks[taskId].data[3] = 0;
+        gTasks[taskId].data[4] = 0;
+
+        StringCopy(gStringVar2, gText_DigitIndicator[gTasks[taskId].data[4]]);
+        ConvertIntToDecimalStringN(gStringVar1, gTasks[taskId].data[3], STR_CONV_MODE_LEADING_ZEROS, 2);
+        StringCopyPadded(gStringVar1, gStringVar1, CHAR_SPACE, 15);
+        StringExpandPlaceholders(gStringVar4, gText_ItemQuantity);
+        AddTextPrinterParameterized(gTasks[taskId].data[2], 1, gStringVar4, 1, 1, 0, NULL);
+
+        gTasks[taskId].func = GiveItem_SelectItemQuantity;
+    }
+    else if (gMain.newKeys & B_BUTTON)
+    {
+        PlaySE(SE_SELECT);
+        GiveItem_DestroySelectItem(taskId);
+    }
+}
+
+static void GiveItem_SelectItemQuantity(u8 taskId)
+{
+    if (gMain.newKeys & DPAD_ANY)
+    {
+        PlaySE(SE_SELECT);
+
+        if(gMain.newKeys & DPAD_UP)
+        {
+            gTasks[taskId].data[3] += sPowersOfTen[gTasks[taskId].data[4]];
+            if(gTasks[taskId].data[3] >= 100){
+                gTasks[taskId].data[3] = 99;
+            }
+        }
+        if(gMain.newKeys & DPAD_DOWN)
+        {
+            gTasks[taskId].data[3] -= sPowersOfTen[gTasks[taskId].data[4]];
+            if(gTasks[taskId].data[3] < 0){
+                gTasks[taskId].data[3] = 0;
+            }
+        }
+        if(gMain.newKeys & DPAD_LEFT)
+        {
+            if(gTasks[taskId].data[4] > 0)
+            {
+                gTasks[taskId].data[4] -= 1;
+            }
+        }
+        if(gMain.newKeys & DPAD_RIGHT)
+        {
+            if(gTasks[taskId].data[4] < 2)
+            {
+                gTasks[taskId].data[4] += 1;
+            }
+        }
+
+        StringCopy(gStringVar2, gText_DigitIndicator[gTasks[taskId].data[4]]);
+        ConvertIntToDecimalStringN(gStringVar1, gTasks[taskId].data[3], STR_CONV_MODE_LEADING_ZEROS, 2);
+        StringCopyPadded(gStringVar1, gStringVar1, CHAR_SPACE, 15);
+        StringExpandPlaceholders(gStringVar4, gText_ItemQuantity);
+
+        AddTextPrinterParameterized(gTasks[taskId].data[2], 1, gStringVar4, 1, 1, 0, NULL);
+    }
+
+    if (gMain.newKeys & A_BUTTON)
+    {
+        PlaySE(MUS_FANFA4);
+        AddBagItem(gTasks[taskId].data[5], gTasks[taskId].data[3]);
+        GiveItem_DestroySelectItem(taskId);
+    }
+    else if (gMain.newKeys & B_BUTTON)
+    {
+        PlaySE(SE_SELECT);
+        GiveItem_DestroySelectItem(taskId);
+    }
+}
+
+static void GiveItem_DestroySelectItem(u8 taskId)
+{
+
+    ClearStdWindowAndFrame(gTasks[taskId].data[1], TRUE);
+    RemoveWindow(gTasks[taskId].data[1]);
+
+    ClearStdWindowAndFrame(gTasks[taskId].data[2], TRUE);
+    RemoveWindow(gTasks[taskId].data[2]);
+
+    DestroyTask(taskId);
+    EnableBothScriptContexts();
+}
+
+static void DebugAction_HealParty(u8 taskId)
+{
+    PlaySE(SE_KAIFUKU);
+    HealPlayerParty();
+    Debug_DestroyMainMenu(taskId);
+}
+
+static void DebugAction_AccessPC(u8 taskId)
+{
+}
+
+static void DebugAction_GivePokemon(u8 taskId)
+{
 }
 
 static void DebugAction_Cancel(u8 taskId)
